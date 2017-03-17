f7c782097767cc2d3a03235131f1a8031ef2884c
Swapnil Verma <swapnil28verma@gmail.com>
2017-03-18 00:21:45 +0530

    Additional Levels Added
    Added World 2(Camera) to the Game
    Level Testing remaining

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,14 +7,21 @@ public class CameraController : MonoBehaviour {
 	private Vector3 to;
 	public float speed = 1.0f; 
 	public float angle = 0;
+	public bool singleRotate;
 
 	void Start() {
-		from = new Vector3(0, -angle, 0);
-		to = new Vector3(0, angle, 0);
+		from = new Vector3(0, this.transform.parent.rotation.eulerAngles.y - angle, 0);
+		to = new Vector3(0, this.transform.parent.rotation.eulerAngles.y + angle, 0);
 	}
 
 	void Update() {
-		float t = Mathf.PingPong(Time.time * speed * 2.0f, 1.0f);
-		transform.eulerAngles = Vector3.Lerp (from, to, t);
+		if (singleRotate) {
+//			transform.Rotate(new Vector3(0, transform.rotation.eulerAngles.y + 1, 0));
+			transform.Rotate(0, Time.deltaTime * speed * 100, 0);
+		}
+		else {
+			float t = Mathf.PingPong(Time.time * speed * 2.0f, 1.0f);
+			transform.eulerAngles = Vector3.Lerp (from, to, t);
+		}
 	}
 }
