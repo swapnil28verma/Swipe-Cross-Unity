1f6ad9d9f4f4c0fd9e5da17f3c7376af8f7225ea
Swapnil Verma <swapnil28verma@gmail.com>
2017-05-04 16:44:03 +0530

    Added pause menu
    Added pause menu with full functionality
    Menu still glitchy though.
    1. Pause button not visible after quit and reloading new level
    2. Player control not working after quitting and reloading new level

---
+++ Assets/PauseController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour {
+
+	public Canvas pauseMenu;
+
+	public Button resume;
+	public Button restart;
+	public Button quit;
+
+	public Image pause;
+
+	public Sprite volSprite, volMuteSprite;
+
+	public Toggle volume;
+
+	private bool isMute = false;
+
+	public void Start() {
+		pauseMenu.enabled = false;
+
+	}
+
+	public void pausePress() {
+		Time.timeScale = 0f;
+		pause.enabled = false;
+		pauseMenu.enabled = true;
+		Debug.Log ("Pause pressed");
+	}
+
+	public void resumePress() {
+		Time.timeScale = 1f;
+		pause.enabled = true;
+		pauseMenu.enabled = false;
+
+	}
+
+	public void volumePress() {
+
+		if (isMute) {
+
+			volume.GetComponent<Image> ().sprite = volSprite;
+		} else {
+			volume.GetComponent<Image> ().sprite = volMuteSprite;
+		}
+		isMute = !isMute;
+
+	}
+
+	public void restartPress() {
+
+		SceneManager.LoadScene (SceneManager.GetActiveScene().name);
+	}
+
+	public void quitPress() {
+
+		SceneManager.LoadScene ("MainMenu");
+	}
+
+
+}
